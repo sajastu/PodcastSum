7951fdf83d971a4782eaf3b0f061b1923778ae10
sajastu <sajad@ir.cs.georgetown.edu>
2022-06-20 12:46:51 -0400

    commit

--- model_train.sh
+++ model_train.sh
@@ -1,4 +1,4 @@
-python run.py --do_train --mode ext --window_type token --parallel --kernel_size 256 --stride 256
+#python run.py --do_train --mode ext --window_type token --parallel --kernel_size 256 --stride 256
 python run.py --do_train --mode abs --window_type token --parallel --kernel_size 256 --stride 256
 python run.py --do_train --mode swh --window_type token --parallel --kernel_size 256 --stride 256
 python run.py --do_train --mode ret --window_type token --parallel --kernel_size 256 --stride 256
